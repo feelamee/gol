20d1cfd3f2c4e57b39bbebed049c604e100e5322
feelamee <fosterlounch@gmail.com>
2026-07-08 02:07:24 +0300

    extract dependencies declaration to separate file

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,21 @@
+include_guard()
+
+if (${GOL_USE_SYSTEM_LIBRARIES})
+    set(CPM_USE_LOCAL_PACKAGES ON)
+    set(CPM_LOCAL_PACKAGES_ONLY ON)
+endif()
+
+include(CPM)
+CPMAddPackage(
+  NAME SDL3
+  GIT_REPOSITORY "https://github.com/libsdl-org/SDL"
+  GIT_TAG release-3.4.10
+  OPTIONS "SDL_ASAN ${GOL_SANITIZERS}"
+)
+CPMAddPackage(
+  NAME glm
+  GIT_REPOSITORY "https://github.com/g-truc/glm"
+  GIT_TAG 1.0.3
+  OPTIONS "GLM_ENABLE_CXX_20 ON"
+)
+
